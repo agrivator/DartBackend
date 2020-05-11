405b00ab95bb8542938ec0869392ed9381cd185d
Ann <annphilomina246@gmail.com>
2020-05-11 23:38:35 +0530

    model serializer for cart model

--- users/serializers.py
+++ users/serializers.py
@@ -8,3 +8,10 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('__all__')
+
+
+class CartSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Cart
+        fields = ('__all__')
